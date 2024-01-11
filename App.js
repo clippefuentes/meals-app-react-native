be6de1a0137e320b80ab7c9af4deca5d4f5f6908
Clyne Fuentes <clippefuentes@gmail.com>
2024-01-10 23:06:17 -0600

    add category screen

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import CategoryScreen from './screens/CategoryScreen';
+import { CATEGORIES } from './data/dummy-data';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {/* <View style={styles.container}> */}
+        <CategoryScreen categories={CATEGORIES} />
+      {/* </View> */}
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 1,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
---
+++ screens/CategoryScreen.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { FlatList, View, Text, StyleSheet, Pressable, Platform } from 'react-native';
+
+const CategoryScreen = ({ categories }) => {
+  const renderCategoryItem = ({ item }) => (
+    <Pressable
+      style={({ pressed }) => [
+        styles.categoryItem,
+        {
+          backgroundColor: pressed ? '#e0e0e0' : item.color,
+        },
+      ]}
+      onPress={() => {
+        console.log(`Pressed on category: ${item.title}`);
+      }}
+    >
+      <Text>{item.title}</Text>
+    </Pressable>
+  );
+
+  return (
+    <View 
+      style={[
+        styles.container,
+      ]}
+    >
+      <FlatList
+        data={categories}
+        renderItem={renderCategoryItem}
+        keyExtractor={(item) => item.id}
+        numColumns={2}
+      />  
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 40,
+    height: '100%',
+  },
+  categoryItem: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+    padding: 16,
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 10,
+    height: 160,
+    shadowColor: 'black',
+    shadowOpacity: 0.25,
+    shadowOffset: {
+      height: 2, width: 0,
+    },
+    shadowRadius: 8,
+    overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
+  },
+});
+
+export default CategoryScreen;
